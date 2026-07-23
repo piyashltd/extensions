9b8141babe8f26b9e9727d264b5e860293dab6ce
Piyashltd <piyashmia42@gmail.com>
2026-07-23 14:05:27 +0600

    Fixed gradle plugin issue

--- MyMovieProvider/build.gradle.kts
+++ MyMovieProvider/build.gradle.kts
@@ -2,7 +2,6 @@ apply(plugin = "com.lagradost.cloudstream3.gradle")
 
 cloudstream {
     name = "Piyash Collection"
-    description = "আমার নিজস্ব মুভি ও সিরিজের কালেকশন"
-    authors = listOf("Piyash")
+    description = "Piyash Custom Repo"
     version = 1
 }
